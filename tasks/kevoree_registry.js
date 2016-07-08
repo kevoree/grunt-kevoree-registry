9b4c6ff669c2da5b1755b391f984110f50bfd00a
Maxime Tricoire <max.tricoire@gmail.com>
2016-07-08 16:06:35 +0200

    WIP toward new registry

--- tasks/kevoree_registry.js
+++ tasks/kevoree_registry.js
@@ -1,96 +1,168 @@
-/*
- * grunt-kevoree-registry
- * https://github.com/kevoree/kevoree-js
- *
- * Copyright (c) 2014 Maxime Tricoire
- * Licensed under the LGPL-3.0 license.
- */
-
 'use strict';
-var registry = require('kevoree-registry-client');
-var kevoree  = require('kevoree-library').org.kevoree;
 
-module.exports = function(grunt) {
+var api = require('kevoree-registry-api');
+var nconf = require('nconf');
+var kevoree = require('kevoree-library').org.kevoree;
 
-    grunt.registerMultiTask('kevoree_registry', 'Grunt plugin that handles POST of Kevoree models to registry.kevoree.org using kevoree-registry-client (you can use --kevoree-registry-host and --kevoree-registry-port to target an alternative registry).', function() {
-        var done = this.async();
+var auth = require('../lib/auth');
+var diff = require('../lib/diff');
 
-        // Merge task-specific and/or target-specific options with these defaults.
-        var options = this.options({
-            type: 'json',
-            host: grunt.option('kevoree-registry-host') || 'registry.kevoree.org',
-            port: grunt.option('kevoree-registry-port') || 80
-        });
+function genUrl() {
+  var host = nconf.get('registry:host');
+  var port = nconf.get('registry:port');
+  var protocol = nconf.get('registry:ssl') ? 'https://' : 'http://';
+  return protocol + host + ((port === 80) ? '' : ':' + port);
+}
 
-        var factory = new kevoree.factory.DefaultKevoreeFactory();
-        var compare = factory.createModelCompare();
-        var loader = factory.createJSONLoader();
-        var serializer = factory.createJSONSerializer();
-        var model = factory.createContainerRoot();
-        factory.root(model);
+module.exports = function (grunt) {
+  grunt.registerMultiTask(
+    'kevoree_registry',
+    'Grunt plugin to publish Kevoree models to a Kevoree Registry (you can use --kevoree-registry-host and --kevoree-registry-port to target an alternative registry).',
+    function () {
+      var done = this.async();
 
-        // Iterate over all specified file groups.
-        this.files.forEach(function(f) {
-            var merged = [];
-            // Concat specified files.
-            f.src.filter(function(filepath) {
-                // Warn on and remove invalid source files (if nonull was set).
-                if (!grunt.file.exists(filepath)) {
-                    grunt.log.warn('Source file "' + filepath + '" not found.');
-                    return false;
-                } else {
-                    return true;
-                }
-            }).map(function(filepath) {
-                // Read file source.
-                var modelData = grunt.file.read(filepath);
-                try {
-                    var fileModel = loader.loadModelFromString(modelData).get(0);
-                    var mergeSeq = compare.merge(model, fileModel);
-                    mergeSeq.applyOn(model);
-                    merged.push(filepath);
-                } catch (err) {
-                    done(new Error('Unable to load model from file "'+filepath+'"'));
-                }
-            });
+      // Merge task-specific and/or target-specific options with these defaults.
+      var options = this.options({
+        registry: {
+          host: grunt.option('kevoree-registry-host') || 'registry.kevoree.org',
+          port: grunt.option('kevoree-registry-port') || 80,
+          ssl: true
+        },
+        user: {}
+      });
+      var config = nconf.argv().env();
+      config.use('memory');
+      config.set('registry', options.registry);
+      config.set('user', options.user);
+      config.set('oauth', {
+        client_secret: 'kevoree_registryapp_secret',
+        client_id: 'kevoree_registryapp'
+      });
 
-            if (merged.length === 0) {
-                done(new Error('No model found'));
-            } else {
-                if (merged.length === 1) {
-                    grunt.log.writeln('Model: ' + merged[0]['blue']);
-                } else {
-                    grunt.log.writeln('Models: ' + merged.join(', ')['blue']);
-                }
+      var url = genUrl();
+      grunt.log.ok('Registry: ' + url.blue);
 
-                var url = options.host + ((options.port !== 80) ? ':' + options.port : '');
-                grunt.log.writeln('Registry: '+'http://'['blue']+url['blue']);
+      var factory = new kevoree.factory.DefaultKevoreeFactory();
+      var compare = factory.createModelCompare();
+      var loader = factory.createJSONLoader();
+      var serializer = factory.createJSONSerializer();
 
-                try {
-                    var regOpts = {
-                        host: options.host,
-                        port: options.port,
-                        model: serializer.serialize(model),
-                        type: options.type
-                    };
+      if (this.files.length === 1) {
+        var filepath = this.files[0].src[0];
+        if (!grunt.file.exists(filepath)) {
+          done(new Error('Model file "' + filepath + '" not found.'));
+        } else {
+          var modelStr = grunt.file.read(filepath);
+          var model;
+          try {
+            model = loader.loadModelFromString(modelStr).get(0);
+          } catch (err) {
+            done(new Error('Unable to load model from file "' + filepath + '"'));
+            return;
+          }
 
-                    try {
-                        registry.post(regOpts, function (err) {
-                            if (err) {
-                                done(err);
-                            } else {
-                                grunt.log.ok('Model successfully published');
-                                done();
-                            }
-                        });
-                    } catch (err) {
-                        done(new Error('Unable to publish the model'));
-                    }
-                } catch (err) {
-                    done(new Error('Unable to serialize the model'));
-                }
+          grunt.log.ok('Model:    ' + filepath.blue);
+          var tdefs = model.select('**/typeDefinitions[]').array;
+          if (!tdefs || tdefs.length > 1) {
+            done(new Error('Model must define one TypeDefinition strictly (found: ' + tdefs.length + ')'));
+            return;
+          } else {
+            var tdefStr;
+            try {
+              tdefs[0].removeAllDeployUnits();
+              tdefStr = serializer.serialize(tdefs[0]);
+            } catch (err) {
+              done(new Error('Unable to serialize TypeDefinition ' + tdefs[0].name + '/' + tdefs[0].version));
+              return;
             }
-        });
-    });
 
+            grunt.log.writeln();
+            grunt.log.writeln('Looking for ' + (tdefs[0].eContainer().name + '.' +
+              tdefs[0].name + '/' + tdefs[0].version).bold + ' in the registry...');
+            api.tdef({
+                namespace: tdefs[0].eContainer().name,
+                name: tdefs[0].name,
+                version: tdefs[0].version
+              })
+              .get()
+              .then(function (tdef) {
+                grunt.log.ok('Found (id:' + tdef.id + ')');
+                grunt.log.writeln();
+                grunt.verbose.writeln('Loading model...');
+                var regTdef;
+                try {
+                  regTdef = loader.loadModelFromString(tdef.model).get(0);
+                  // create a ContainerRoot for registry tdef
+                  var regModel = factory.createContainerRoot().withGenerated_KMF_ID(0);
+                  factory.root(regModel);
+                  var regPkg = factory.createPackage();
+                  regPkg.name = tdef.namespace.name;
+                  regModel.addPackages(regPkg);
+                  regPkg.addTypeDefinitions(regTdef);
+                  // create a ContainerRoot for src tdef
+                  var srcModel = factory.createContainerRoot().withGenerated_KMF_ID(0);
+                  factory.root(srcModel);
+                  var srcPkg = factory.createPackage();
+                  srcPkg.name = tdef.namespace.name;
+                  srcModel.addPackages(srcPkg);
+                  srcPkg.addTypeDefinitions(tdefs[0]);
+                  // diff the two models to ensure there are the same
+                  var traces = compare.diff(regModel, srcModel).traces.array;
+                  if (traces.length === 0) {
+                    // no diff between local and registry: good to go
+                    done();
+                  } else {
+                    // there is differences between local and registry: error
+                    diff(grunt, srcModel, regModel, traces);
+                    done(new Error('If you want to use your local changes then you have to increment your TypeDefinition version.'));
+                  }
+                } catch (err) {
+                  done(err);
+                }
+              })
+              .catch(function (err) {
+                if (err.code === 404) {
+                  // typeDef does not exist: create it
+                  grunt.log.warn('Not found.');
+                  grunt.log.writeln('Trying to add ' + (tdefs[0].eContainer().name + '.' +
+                    tdefs[0].name + '/' + tdefs[0].version).bold + ' in the registry...');
+
+                  auth(grunt)
+                    .then(function () {
+                      grunt.verbose.writeln('TypeDefinition Model: ' + JSON.stringify(JSON.parse(tdefStr), null, 2));
+                      api.tdef({
+                          namespace: tdefs[0].eContainer().name,
+                          name: tdefs[0].name,
+                          version: tdefs[0].version,
+                          model: tdefStr
+                        })
+                        .create()
+                        .then(function () {
+                          grunt.log.ok('Success:  ' + (tdefs[0].eContainer().name + '.' +
+                            tdefs[0].name + '/' + tdefs[0].version).bold + ' published on registry');
+                        })
+                        .catch(function (err) {
+                          if (err.code === 401) {
+                            grunt.log.warn('You are not logged in');
+                          } else if (err.code === 403) {
+                            grunt.log.warn('You are not a member of namespace "' + tdefs[0].eContainer().name + '"');
+                          } else {
+                            grunt.log.warn(err.message);
+                          }
+                          done(err);
+                        });
+                    })
+                    .catch(function (err) {
+                      done(err);
+                    });
+                } else {
+                  done(err);
+                }
+              });
+          }
+        }
+      } else {
+        done(new Error('You must specify one Kevoree model JSON file'));
+      }
+    });
 };
